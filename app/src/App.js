243f9ff2169a04705a8e8b51964ee339bb7e2a08
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-09-22 17:21:42 +0200

    proof of good rooting except for page stack-up

--- app/src/App.js
+++ app/src/App.js
@@ -28,112 +28,14 @@ const opts = {
 };
 const programID = new PublicKey(idl.metadata.address);
 
-function App() {
-  const [value, setValue] = useState("");
-  const [dataList, setDataList] = useState([]);
-  const [input, setInput] = useState("");
-  const wallet = useWallet();
-
-  async function getProvider() {
-    /* create the provider and return it to the caller */
-    /* network set to local network for now */
-    const network = "http://127.0.0.1:8899";
-    const connection = new Connection(network, opts.preflightCommitment);
-
-    const provider = new Provider(connection, wallet, opts.preflightCommitment);
-    return provider;
-  }
-
-  async function initialize() {
-    const provider = await getProvider();
-    /* create the program interface combining the idl, program ID, and provider */
-    const program = new Program(idl, programID, provider);
-    try {
-      /* interact with the program via rpc */
-      await program.rpc.initialize("Hello World", {
-        accounts: {
-          baseAccount: baseAccount.publicKey,
-          user: provider.wallet.publicKey,
-          systemProgram: SystemProgram.programId,
-        },
-        signers: [baseAccount],
-      });
-
-      const account = await program.account.baseAccount.fetch(
-        baseAccount.publicKey
-      );
-      console.log("account: ", account);
-      setValue(account.data.toString());
-      setDataList(account.dataList);
-    } catch (err) {
-      console.log("Transaction error: ", err);
-    }
-  }
-
-  async function update() {
-    if (!input) return;
-    const provider = await getProvider();
-    const program = new Program(idl, programID, provider);
-    await program.rpc.update(input, {
-      accounts: {
-        baseAccount: baseAccount.publicKey,
-      },
-    });
-
-    const account = await program.account.baseAccount.fetch(
-      baseAccount.publicKey
-    );
-    console.log("account: ", account);
-    setValue(account.data.toString());
-    setDataList(account.dataList);
-    setInput("");
-  }
-
-  if (!wallet.connected) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "100px",
-        }}
-      >
-        <WalletMultiButton />
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <div>
-          {!value && <button onClick={initialize}>Initialize</button>}
-
-          {value ? (
-            <div>
-              <h2>Current value: {value}</h2>
-              <input
-                placeholder="Add new data"
-                onChange={(e) => setInput(e.target.value)}
-                value={input}
-              />
-              <button onClick={update}>Add data</button>
-            </div>
-          ) : (
-            <h3>Please Inialize.</h3>
-          )}
-          {dataList.map((d, i) => (
-            <h4 key={i}>{d}</h4>
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
-
 const AppWithProvider = () => (
   <ConnectionProvider endpoint="http://127.0.0.1:8899">
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
-        <App />
+        <BrowserRouter>
+          <Route path="/" component={Home} />
+          <Route path="/marketplace" component={Marketplace} />
+        </BrowserRouter>
       </WalletModalProvider>
     </WalletProvider>
   </ConnectionProvider>
