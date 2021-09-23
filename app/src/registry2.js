1f0d5465126be57a96d2015c4d6b9d0112b6b967
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-09-23 17:56:13 +0200

    changed table to denstable

--- app/src/registry2.js
+++ app/src/registry2.js
@@ -1,10 +1,10 @@
-import Table from "./components/Table";
+import DenseTable from "./components/DenseTable";
 import "./App.css";
 export default function Registry2() {
   return (
     <div>
       Welcome to the registry
-      <Table />
+      <DenseTable />
     </div>
   );
 }
