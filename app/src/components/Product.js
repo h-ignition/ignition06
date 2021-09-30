5c1c461b5b0436b60ab47b79c9e07a64b6c7ea7b
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-09-30 12:20:31 +0200

    rewinded

--- app/src/components/Product.js
+++ app/src/components/Product.js
@@ -1,5 +1,4 @@
 //this needs to be Product(name) or Product (hash)
-
 function Product() {
   return <div>Hello this is the test product</div>;
 }
--- app/src/productPage.js
+++ app/src/productPage.js
@@ -1,11 +1,1 @@
 //this needs dynamic routing
-
-//this needs to be Product(name) or Product (hash)
-import Product from "./components/Product";
-export default function ProductPage(props) {
-  return (
-    <div>
-      Hello this is the test product<Product></Product>
-    </div>
-  );
-}
