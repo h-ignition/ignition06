29fa8e4e7b0fa671b3846a3751307c50e06149b8
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-10-13 12:02:48 +0200

    redefined program consts

--- app/src/components/DenseTableV1.js
+++ app/src/components/DenseTableV1.js
@@ -7,11 +7,40 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useState } from "react";
-import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
-import { BN, Program, Provider, web3 } from "@project-serum/anchor";
+import {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  SystemProgram,
+} from "@solana/web3.js";
+import {
+  BN,
+  Program,
+  Provider,
+  web3,
+  setProvider,
+} from "@project-serum/anchor";
 import idl2 from "../idl2.json";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  ensureBalance,
+  getCandyMachine,
+  getCandyProgram,
+  getHarmoniaProgram,
+  getMasterEditionAddress,
+  getMetadataAddress,
+  getOwnedTokenAccounts,
+  getTokenWalletAddress,
+  initializeCandyMachine,
+  mintNft,
+  TOKEN_METADATA_PROGRAM_ID,
+  updateCandyMachine,
+} from "../utils/helper";
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 
 const { Keypair } = web3;
 const baseAccount = Keypair.generate();
@@ -91,7 +120,7 @@ export default function DenseTable() {
       }
     );
   }
-  async function buy(amount) {
+  /*async function buy(amount) {
     const provider = await getProvider();
     const program = new Program(idl2, programID, provider);
 
@@ -109,8 +138,59 @@ export default function DenseTable() {
     alert(
       `thanks for your purchase, an nft lvl ${q} will be added to your Solana wallet`
     );
-  }
+  }*/
+  async function buyAndMint(offsets) {
+    ////////////////////////////////////////////////////////////////////////
+    const provider = await getProvider();
+    setProvider(provider);
+    const sellerAccount = web3.Keypair.generate();
+    const projectAccount = "6qthogdMfaYtdeLrfaCfFtYQAiouRoPpaWsgS7nDoNkH";
+    const buyerAccount = web3.Keypair.generate();
+    const harmoniaProgram = new Program(idl2, programID, provider);
+    const candyProgram = new Program(idl, programID, provider);
+    const candyProgramId = candyProgram.programId;
+    console.log(`Connecting to ${provider.connection["_rpcEndpoint"]}`);
+    let config = null;
+    let candyMachineUuid = null;
+    /////////////////////////////////////////////////////////////
+    const mint = web3.Keypair.generate();
+    const [candyMachine, bump] = await getCandyMachine(
+      config.publicKey,
+      candyMachineUuid,
+      candyProgramId
+    );
+    const token = await getTokenWalletAddress(
+      buyerAccount.publicKey,
+      mint.publicKey
+    );
+    const metadata = await getMetadataAddress(mint.publicKey);
+    const masterEdition = await getMasterEditionAddress(mint.publicKey);
+    const tx = await harmoniaProgram.rpc.buyAndMint(new BN(offsets), {
+      accounts: {
+        project: projectAccount.publicKey,
+        buyer: buyerAccount.publicKey,
+        seller: sellerAccount.publicKey,
+        candyProgram: candyProgram.programId,
 
+        config: config.publicKey,
+        candyMachine: candyMachine,
+        payer: buyerAccount.publicKey,
+        wallet: sellerAccount.publicKey, // treasury
+        mint: mint.publicKey,
+        associatedToken: token,
+        metadata: metadata,
+        masterEdition: masterEdition,
+        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+        ataProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+        rent: web3.SYSVAR_RENT_PUBKEY,
+        clock: web3.SYSVAR_CLOCK_PUBKEY,
+      },
+      signers: [mint, buyerAccount],
+    });
+    return tx;
+  }
   if (!wallet.connected) {
     return (
       <div
@@ -137,10 +217,10 @@ export default function DenseTable() {
                 <TableCell align="right">owner</TableCell>
                 <button
                   onClick={() => {
-                    buy(1);
+                    buyAndMint(1);
                   }}
                 >
-                  purchase 10
+                  purchase 1
                 </button>
               </TableRow>
             </TableHead>
