0c799db056df3552f5761b8338eca7ae0e2eb5af
Bigandy4climate <samuelnihoul@hotmail.fr>
2021-10-14 16:17:40 +0200

    ..

--- app/src/utils/getProvider.js
+++ app/src/utils/getProvider.js
@@ -1,7 +1,3 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-
 import { Connection, PublicKey } from "@solana/web3.js";
 import { Provider, web3 } from "@project-serum/anchor";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -10,7 +6,7 @@ const opts = {
 };
 export default async function getProvider() {
   const wallet = useWallet();
-  const network = "http://127.0.0.1:8899";
+  const network = "devnet";
   const connection = new Connection(network, opts.preflightCommitment);
   const provider = new Provider(connection, wallet, opts.preflightCommitment);
   return provider;
